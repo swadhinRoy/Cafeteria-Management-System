38e7d965dc8cb3ed9b08607191296bfd565ee715
Swadhin Roy <66383159+swadhinRoy@users.noreply.github.com>
2020-09-28 01:01:51 +0600

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,8 @@
 <?php
+include_once 'TestCode/function.php';
+
 $con = mysqli_connect("localhost","root","")or die("Unable to connect");
 mysqli_select_db($con,"beta");
+
+connectionTest($con); //added here
 ?>
